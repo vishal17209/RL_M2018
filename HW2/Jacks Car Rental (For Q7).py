c9b53d8dd912f5c0ae29421898982194ac09b16c
vishal17209 <vishal17209@iiitd.ac.in>
2019-09-11 23:16:54 +0530

    udpates

--- HW2/Jacks Car Rental (For Q7).py
+++ HW2/Jacks Car Rental (For Q7).py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[91]:
 
-
-get_ipython().run_line_magic('matplotlib', 'inline')
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from math import factorial
 
 
-# In[ ]:
+# In[92]:
+
+
+#utitlity functions
+def isterminal(state, size):
+    if(state == (0,0) or state == (size-1,size-1)):
+        return True
+    return False
+
+def poisson(n,lam):
+    prob = ((lam**n)*np.exp(-lam))/factorial(n)
+    return prob
+
+
+# In[1]:
 
 
 class DP:
@@ -19,21 +32,46 @@ class DP:
         self.actions = actions
         self.discount = discount
         self.policy = policy
+        self.lambda_rental1 = 3
+        self.lambda_rental2 = 4
+        self.lambda_return1 = 3
+        self.lambda_return2 = 2
     
     def printPolicy(self):
         for i in self.policy:
             print(self.policy[i])
         print()
         
-    def getNextState(self, state, action):
-        if(isterminal(state, self.size)):
-            return state, 0
-        else:
-            next_state = tuple(np.array(state) + np.array(action))
-            if(next_state[0] < self.size and next_state[1] < self.size and next_state[0] >= 0 and next_state[1] >= 0):
-                return next_state, -1
-            else:
-                return state, -1
+    def getReturn(self, state, values, action):
+        returns = -2*np.abs(action)
+        
+        carsAt1 = min(state[0] + action, self.size-1) 
+        carsAt2 = min(state[1] - action, self.size-1)
+        
+        for rental1 in range(11):
+            for rental2 in range(11):
+                prob = poisson(rental1, self.lambda_rental1)*poisson(rental2, self.lambda_rental2)
+                
+                available1 = min(rental1, carsAt1)
+                available2 = min(rental2, carsAt2)
+                
+                rewards = (available1  + available2) * 10
+                
+                for return1 in range(11):
+                    for return2 in range(11):
+                        prob *= poisson(return1, self.lambda_return1)*poisson(return2, self.lambda_return2)
+                        
+                        carAt1EOD = min(max(carsAt1 - rental1 + return1, 0), self.size-1)
+                        carAt2EOD = min(max(carsAt2 - rental2 + return2, 0), self.size-1)
+
+                        returns += prob*(rewards + self.discount*values[carAt1EOD, carAt2EOD])
+        return returns  
+    
+    def getAllReturns(self, state, values):
+        action_values = np.zeros(len(self.actions))
+        for a in range(len(self.actions)):
+            action_values[a] += self.getReturn(state, values, self.actions[a])
+        return action_values
         
     def policy_iteration(self):
         new_v =  np.zeros((self.size, self.size))
@@ -49,24 +87,21 @@ class DP:
                         state = (i,j)
                         v = 0
                         for a in range(len(self.actions)):
-                            new_state, reward = self.getNextState(state, self.actions[a])
-                            v += self.policy[state][a]*(reward + self.discount*values[new_state[0], new_state[1]])
+                            v+= self.getReturn(state, values, self.actions[a])
                         delta = max(delta, np.abs(v - new_v[i,j]))
                         new_v[i,j] = v
                 values = new_v
-                
-                if(delta < 1e-5):
+                print(delta)
+                if(delta < 1e-2):
                     break
-            
+
             #policy improvement
             policy_stable = True
             for i in range(self.size):
                 for j in range(self.size):
                     action_values = np.zeros(len(self.actions))
                     state = (i,j)
-                    for a in range(len(self.actions)):
-                        new_state , reward = self.getNextState(state, self.actions[a])
-                        action_values[a] += (reward + self.discount*new_v[new_state[0], new_state[1]])
+                    action_values = self.getAllReturns(state, values)
                     old_action = np.argmax(self.policy[state])
                     new_action = np.argmax(action_values)
     
@@ -75,9 +110,37 @@ class DP:
                     self.policy[state] = np.eye(4)[new_action]
 
             if(policy_stable):
-                print()
                 return self.policy, new_v
                 
+
+
+# In[94]:
+
+
+max_cars = 20
+max_car_moves = 5
+
+fake_policy = {}
+
+#taking positive actions as addition to location 1 and negative actions as addition to location 2
+actions = []
+for i in range(-5,6,1):
+    actions.append(i)
+
+for i in range(max_cars+1):
+    for j in range(max_cars+1):
+        fake_policy[(i,j)] = np.ones(len(actions))/len(actions)
+
+dp = DP(max_cars+1, actions, fake_policy, 0.9)
+
+policy, v = dp.policy_iteration()
+
+for i in policy:
+    print(policy)
+print()
+
+print(np.round(v))
+print()
 
 
 # In[ ]:
